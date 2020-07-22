592d7916e625ac44314a4c9be3d492b2ddb34514
nu11secur1ty <venvaropt@gmail.com>
2020-07-22 16:37:01 +0300

    Update mujkatolainovduj.pl

--- Tools/mujkatolainovduj.pl
+++ Tools/mujkatolainovduj.pl
@@ -6,5 +6,7 @@ use diagnostics;
 
 my $open_VAS = `docker pull nu11secur1ty/ovas-ops:latest`;
   system($open_VAS);
+    print "$open_VAS\n";
 my $run_VAS = `docker run --detach --publish 8080:9392 --env PASSWORD="password" --volume gvm-data:/data --name gvm nu11secur1ty/ovas-ops:latest`;
   system($run_VAS);
+    print "$run_VAS\n";
