af8e19a0e5702f04b4507e1883502165c60abfd0
nu11secur1ty <venvaropt@gmail.com>
2020-07-24 01:18:57 +0300

    Update mujkatolainovduj.pl

--- Tools/mujkatolainovduj.pl
+++ Tools/mujkatolainovduj.pl
@@ -5,5 +5,4 @@ use warnings;
 use diagnostics;
 
 my $package = `apt-get install -y curl wget git`;
-my $get = `curl -o docker-compose.yml https://raw.githubusercontent.com/skydive-project/skydive/master/contrib/docker/docker-compose.yml`;
-my $run = `docker-compose up`;
+my $get = `docker run -d --privileged --pid=host --net=host -p 8082:8082 -p 8081:8081 -e SKYDIVE_ANALYZER_LISTEN=0.0.0.0:8082 -v /var/run/docker.sock:/var/run/docker.sock -v /run/netns:/var/run/netns skydive/skydive allinone`;
