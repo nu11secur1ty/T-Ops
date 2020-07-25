66d6709e843bda517b365a7b1707b7f75ab95156
nu11secur1ty <venvaropt@gmail.com>
2020-07-25 17:22:20 +0300

    Update mujkatolainovduj.pl

--- Tools/mujkatolainovduj.pl
+++ Tools/mujkatolainovduj.pl
@@ -6,3 +6,4 @@ use diagnostics;
 
 my $package = `apt-get install -y curl wget git`;
 my $get = `docker run -d --privileged --pid=host --net=host -p 8082:8082 -p 8081:8081 -e SKYDIVE_ANALYZER_LISTEN=0.0.0.0:8082 -v /var/run/docker.sock:/var/run/docker.sock -v /run/netns:/var/run/netns skydive/skydive allinone`;
+  system($get);
