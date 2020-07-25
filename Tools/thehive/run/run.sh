c7b710b8d9bf789903cd5ab3cd71108a185f3a0a
nu11secur1ty <venvaropt@gmail.com>
2020-07-25 14:01:37 +0300

    Delete run.sh

--- Tools/thehive/run/run.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/bash
-apt install -y docker docker-compose git wget 
-apt autoremove && apt update -y
-git clone https://github.com/nu11secur1ty/thehive.git
-cd thehive
-docker-compose up &
- exit 0;
