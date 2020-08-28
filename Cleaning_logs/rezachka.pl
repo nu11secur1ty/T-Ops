2b1092f58066ee56b67c56f216981114334ed7b7
nu11secur1ty <venvaropt@gmail.com>
2020-08-29 00:35:25 +0300

    Create rezachka.pl

---
+++ Cleaning_logs/rezachka.pl
@@ -0,0 +1,57 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+use diagnostics;
+
+#adbhoney/log/*  
+#citrixhoneypot/log/*  
+#cowrie/log/*   
+#dionaea/log/*     
+#elk/log/*        
+#ews/log/*   
+#glutton/log/*    
+#honeypy/log/*   
+#honeytrap/log/*  
+#medpot/log/*  
+#p0f/log/*   
+#spiderfoot/log/*  
+#tanner/log/*
+#ciscoasa/log/*  
+#conpot/log/*          
+#dicompot/log/*  
+#elasticpot/log/*  
+#emobility/log/*  
+#fatt/log/*  
+#heralding/log/*  
+#honeysap/log/*  
+#mailoney/log/*   
+#nginx/log/*   
+#rdpy/log/*  
+#suricata/log/*
+
+# modules cleaning logs
+my $j = `rm -rf adbhoney/log/*`;
+my $k = `rm -rf citrixhoneypot/log/*`;
+my $l = `rm -rf cowrie/log/*`;
+my $m = `rm -rf dionaea/log/*`;
+my $n = `rm -rf elk/log/*`;
+my $o = `rm -rf ews/log/*`;
+my $p = `rm -rf glutton/log/*`;
+my $q = `rm -rf honeypy/log/*`;
+my $r = `rm -rf honeytrap/log/*`;
+my $s = `rm -rf medpot/log/*`;
+my $t = `rm -rf p0f/log/*`;
+my $u = `rm -rf spiderfoot/log/*`;
+my $v = `rm -rf tanner/log/*`;
+my $w = `rm -rf ciscoasa/log/*`;
+my $x = `rm -rf conpot/log/*`;
+my $y = `rm -rf dicompot/log/*`;
+my $z = `rm -rf elasticpot/log/*`;
+my $as = `rm -rf emobility/log/*`;
+my $bas= `rm -rf fatt/log/*`;
+my $gas = `rm -rf heralding/log/*`;
+my $rfa = `rm -rf honeysap/log/*`;
+my $tras = `rm -rf mailoney/log/*`;
+my $man = `rm -rf nginx/log/*`;
+my $ksh = `rm -rf rdpy/log/*`;
+my $al = `rm -rf suricata/log/*`;
